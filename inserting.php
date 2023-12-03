5ca5dfd5e1b32515fe352a7cbc97381c7f344a4e
Leomar P. De la cruz <146308573+Leomar-Delacruz@users.noreply.github.com>
2023-12-03 22:40:18 +0800

    added new

---
+++ inserting.php
@@ -0,0 +1,28 @@
+<?php
+require_once "database.php";
+
+// Insert new transaction if the form is submitted
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $productName = $_POST['product_name'];
+    $transactionDate = $_POST['transaction_date'];
+    $quantity = $_POST['quantity'];
+    $price = $_POST['price'];
+
+    // Perform validation and sanitation as needed
+
+    // Calculate total amount
+    $totalAmount = $quantity * $price;
+
+    // Insert data into the Transactions table
+    $insertSql = "INSERT INTO transaction (productname, transactiondate, quantity, price, totalamount)
+                  VALUES ('$productName', '$transactionDate', $quantity, $price, $totalAmount)";
+
+    if ($conn->query($insertSql) === TRUE) {
+        echo "New transaction added successfully!";
+    } else {
+        echo "Error: " . $insertSql . "<br>" . $conn->error;
+    }
+}
+
+$conn->close();
+?>
--- transaction.php
+++ transaction.php
@@ -1,27 +1,72 @@
 <?php
 require_once "database.php";
 
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $transactionDate = $_POST['transactionDate'];
-    $productID = $_POST['productID'];
-    $quantity = $_POST['quantity'];
-    $totalAmount = $_POST['totalAmount'];
-    $userID = $_POST['userID'];
-
-    $sql = "INSERT INTO transactions (TransactionDate, ProductID, Quantity, TotalAmount, UserID) VALUES (?, ?, ?, ?, ?)";
-    $stmt = $conn->prepare($sql);
-
-    if (!$stmt) {
-        die('Error preparing statement: ' . $conn->error);
-    }
-
-    $stmt->bind_param('siiid', $transactionDate, $productID, $quantity, $totalAmount, $userID);
-    $stmt->execute();
-
-    echo "Transaction added successfully.";
-
-    $stmt->close();
-}
+// Fetch data from the database
+$sql = "SELECT transactionid, productname, transactiondate, quantity, price, totalamount FROM transaction";
+$result = $conn->query($sql);
 
 $conn->close();
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Transaction Information</title>
+    <style>
+        table {
+            width: 100%;
+            border-collapse: collapse;
+            margin-top: 20px;
+        }
+
+        table, th, td {
+            border: 1px solid black;
+        }
+
+        th, td {
+            padding: 10px;
+            text-align: left;
+        }
+
+        th {
+            background-color: #f2f2f2;
+        }
+    </style>
+</head>
+<body>
+    <h2>Transaction Information</h2>
+    <table>
+        <thead>
+            <tr>
+                <th>TransactionID</th>
+                <th>ProductName</th>
+                <th>TransactionDate</th>
+                <th>Quantity</th>
+                <th>Price</th>
+                <th>TotalAmount</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php
+            // Display data in the HTML table
+            if ($result->num_rows > 0) {
+                while ($row = $result->fetch_assoc()) {
+                    echo "<tr>";
+                    echo "<td>" . $row["transactionid"] . "</td>";
+                    echo "<td>" . $row["productname"] . "</td>";
+                    echo "<td>" . $row["transactiondate"] . "</td>";
+                    echo "<td>" . $row["quantity"] . "</td>";
+                    echo "<td>" . $row["price"] . "</td>";
+                    echo "<td>" . $row["totalamount"] . "</td>";
+                    echo "</tr>";
+                }
+            } else {
+                echo "<tr><td colspan='6'>No transactions found</td></tr>";
+            }
+            ?>
+        </tbody>
+    </table>
+</body>
+</html>
