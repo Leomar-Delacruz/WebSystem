6232680943455f019a461fcbfd678c9e6d12cee8
Ryuo Jhon Mark <143322665+MarkRyuo@users.noreply.github.com>
2023-12-03 22:20:41 +0800

    update here

--- script.js
+++ script.js
@@ -13,9 +13,9 @@ function login() {
           if (response.success) {
               // Redirect to the appropriate page based on the role
               if (response.role === 'admin') {
-                  window.location.href = '/Admin_Dashboard/admin.html';
+                  window.location.href = ''; //Todo
               } else {  
-                  window.location.href = '/Employee_Dashboard/Employee.html';
+                  window.location.href = ''; //Todo
               }
           } else {
               // Display login failure message in the center
