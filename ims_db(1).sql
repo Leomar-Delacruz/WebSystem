bbe9e3b2a618650a124e1b2fa828b718f3c10898
Ryuo Jhon Mark <143322665+MarkRyuo@users.noreply.github.com>
2023-12-03 19:55:25 +0800

    update here

---
+++ ims_db(1).sql
@@ -0,0 +1,124 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1:3306
+-- Generation Time: Dec 03, 2023 at 11:54 AM
+-- Server version: 8.0.31
+-- PHP Version: 8.0.26
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `ims_db`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `products`
+--
+
+DROP TABLE IF EXISTS `products`;
+CREATE TABLE IF NOT EXISTS `products` (
+  `ProductID` int NOT NULL AUTO_INCREMENT,
+  `ProductName` varchar(100) DEFAULT NULL,
+  `Category` varchar(100) DEFAULT NULL,
+  `Description` text,
+  `StockQuantity` int DEFAULT NULL,
+  `PricePerUnit` decimal(10,2) DEFAULT NULL,
+  `SupplierID` int DEFAULT NULL,
+  PRIMARY KEY (`ProductID`),
+  KEY `SupplierID` (`SupplierID`)
+) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `products`
+--
+
+INSERT INTO `products` (`ProductID`, `ProductName`, `Category`, `Description`, `StockQuantity`, `PricePerUnit`, `SupplierID`) VALUES
+(1, 'Coffee Cup', 'Cup', 'Mainit', 200, '5.50', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `suppliers`
+--
+
+DROP TABLE IF EXISTS `suppliers`;
+CREATE TABLE IF NOT EXISTS `suppliers` (
+  `SupplierID` int NOT NULL AUTO_INCREMENT,
+  `SupplierName` varchar(100) DEFAULT NULL,
+  `ContactNumber` varchar(15) DEFAULT NULL,
+  `Email` varchar(100) DEFAULT NULL,
+  `Address` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`SupplierID`)
+) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `suppliers`
+--
+
+INSERT INTO `suppliers` (`SupplierID`, `SupplierName`, `ContactNumber`, `Email`, `Address`) VALUES
+(1, 'JhonMark', '09123456789', 'jhonmark@example.cpm', 'Cumba Mindanao');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `transactions`
+--
+
+DROP TABLE IF EXISTS `transactions`;
+CREATE TABLE IF NOT EXISTS `transactions` (
+  `TransactionID` int NOT NULL AUTO_INCREMENT,
+  `TransactionDate` date DEFAULT NULL,
+  `ProductID` int DEFAULT NULL,
+  `Quantity` int DEFAULT NULL,
+  `TotalAmount` decimal(10,2) DEFAULT NULL,
+  `UserID` int DEFAULT NULL,
+  PRIMARY KEY (`TransactionID`),
+  KEY `ProductID` (`ProductID`)
+) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `transactions`
+--
+
+INSERT INTO `transactions` (`TransactionID`, `TransactionDate`, `ProductID`, `Quantity`, `TotalAmount`, `UserID`) VALUES
+(1, '2023-12-13', 1, 250, '500.00', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `username` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `role` enum('admin','employee') NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `username`, `password`, `role`) VALUES
+(1, 'admin', 'admin', 'admin'),
+(2, 'staff', 'staff', 'employee');
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
