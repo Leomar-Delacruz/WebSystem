c5ad77f3fd6251579b04bbbd46efa9c5daf0fe6a
Ryuo Jhon Mark <143322665+MarkRyuo@users.noreply.github.com>
2023-12-06 21:15:05 +0800

    update here

--- Assets/Staff_Dashboard/direct.js
+++ Assets/Staff_Dashboard/direct.js
@@ -29,7 +29,7 @@
     if (result) {
       // User clicked "OK" (Yes), redirect to a specific URL
       alert("You have been logged out!");
-      window.location.href = "index.php";  
+      window.location.href = "/index.php";  
     } else {
       // User clicked "Cancel", do nothing or handle accordingly
     }
