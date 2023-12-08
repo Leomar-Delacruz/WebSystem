1eb0d03aadcb464337cc010566c6e93843ecfae3
Ryuo Jhon Mark <143322665+MarkRyuo@users.noreply.github.com>
2023-12-08 21:20:38 +0800

    update here

--- transaction.php
+++ transaction.php
@@ -180,7 +180,7 @@ $result = $conn->query($sql);
 <body>
     
         <nav>
-        <button type="button" class="btn btn-success">
+        <button type="button" class="btn btn-success" id="Back">
         Back</button>
         <p class="display-6">Transaction Information</p>
         </nav>
@@ -242,4 +242,13 @@ $result = $conn->query($sql);
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 </body>
+<script>
+    // This is Btn back
+    var Back = document.getElementById("Back");
+
+    Back.addEventListener("click", function() {
+        // Go back to the previous page
+        window.history.back();
+    });
+</script>
 </html>
