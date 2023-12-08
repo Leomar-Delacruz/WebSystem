3455c393045771d7c2daa6bb2dfda37bbd37a2ff
Ryuo Jhon Mark <143322665+MarkRyuo@users.noreply.github.com>
2023-12-08 16:48:20 +0800

    update here

--- product.php
+++ product.php
@@ -42,6 +42,7 @@ $result = $conn->query($sqlSelect);
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/CSS/button-2.css">
     <title>Product Information</title>
     <style>
 
