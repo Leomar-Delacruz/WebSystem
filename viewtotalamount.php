aeec8548d2904b0078af9daaafc80437b76e35e5
Ryuo Jhon Mark <143322665+MarkRyuo@users.noreply.github.com>
2023-12-08 21:00:42 +0800

    update here

--- viewtotalamount.php
+++ viewtotalamount.php
@@ -16,6 +16,9 @@ if ($result === false) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+
     <title>View Collected Total Amounts</title>
     <style>
         body {
@@ -67,9 +70,31 @@ if ($result === false) {
         .back-btn {
             background-color: #333; /* Dark Gray */
         }
+
+        nav {
+            display: flex;
+            justify-content: space-around;
+            align-items: center;
+            width: 100vw;
+            height: 10vh;
+
+            box-shadow: 2px 5px 4px #ddd;
+            
+            /* outline: 1px solid black; */
+        }
+
+        nav p {
+            margin-right: 28vw;
+        }
     </style>
 </head>
 <body>
+
+    <nav>
+        <a href="transaction.php" class="btn btn-success btn-lg">Back</a>
+        <p class="display-6">View Collected Total Amounts</p>
+    </nav>
+
     <h2>View Collected Total Amounts</h2>
     <table>
         <thead>
@@ -97,5 +122,7 @@ if ($result === false) {
     <div class="container">
         <a href="transaction.php" class="btn back-btn">Back to Transactions</a>
     </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 </body>
 </html>
