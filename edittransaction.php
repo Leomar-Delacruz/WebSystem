23dedb6ffd52b4098a4b4451a460f405d7a80acc
Ryuo Jhon Mark <143322665+MarkRyuo@users.noreply.github.com>
2023-12-08 20:43:46 +0800

    update here

--- edittransaction.php
+++ edittransaction.php
@@ -1,65 +1,71 @@
-    <?php
-    require_once "database.php";
+<?php
+require_once "database.php";
 
-    // Check if ID parameter is set
-    if(isset($_GET['id'])) {
-        $transactionID = $_GET['id'];
+// Check if ID parameter is set
+if(isset($_GET['id'])) {
+    $transactionID = $_GET['id'];
 
-        // Retrieve transaction details from the database
-        $sql = "SELECT * FROM transaction WHERE transactionid = ?";
-        
-        $stmt = $conn->prepare($sql);
-        $stmt->bind_param('i', $transactionID);
-        $stmt->execute();
-        $result = $stmt->get_result();
+    // Retrieve transaction details from the database
+    $sql = "SELECT * FROM transaction WHERE transactionid = ?";
+    
+    $stmt = $conn->prepare($sql);
+    $stmt->bind_param('i', $transactionID);
+    $stmt->execute();
+    $result = $stmt->get_result();
 
-        if ($result->num_rows > 0) {
-            $row = $result->fetch_assoc();
-            $productName = $row["productname"];
-            $transactionDate = $row["transactiondate"];
-            $quantity = $row["quantity"];
-            $price = $row["price"];
-            $totalAmount = $row["totalamount"];
-        } else {
-            echo "Transaction not found.";
-            exit();
-        }
+    if ($result->num_rows > 0) {
+        $row = $result->fetch_assoc();
+        $productName = $row["productname"];
+        $transactionDate = $row["transactiondate"];
+        $quantity = $row["quantity"];
+        $price = $row["price"];
+        $totalAmount = $row["totalamount"];
     } else {
-        echo "Transaction ID not provided.";
+        echo "Transaction not found.";
         exit();
     }
+} else {
+    echo "Transaction ID not provided.";
+    exit();
+}
 
-    // Update transaction if the form is submitted
-    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-        $productName = $_POST['product_name'];
-        $transactionDate = $_POST['transaction_date'];
-        $quantity = $_POST['quantity'];
-        $price = $_POST['price'];
+// Update transaction if the form is submitted
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $productName = $_POST['product_name'];
+    $transactionDate = $_POST['transaction_date'];
+    $quantity = $_POST['quantity'];
+    $price = $_POST['price'];
 
-        // Perform validation and sanitation as needed
+    // Perform validation and sanitation as needed
 
-        // Calculate total amount
-        $totalAmount = $quantity * $price;
+    // Calculate total amount
+    $totalAmount = $quantity * $price;
 
-        // Update data in the transaction table
-        $updateSql = "UPDATE transaction 
-                    SET productname = ?, transactiondate = ?, 
-                    quantity = ?, price = ?, totalamount = ?
-                    WHERE transactionid = ?";
+    // Update data in the transaction table
+    $updateSql = "UPDATE transaction 
+                SET productname = ?, transactiondate = ?, 
+                quantity = ?, price = ?, totalamount = ?
+                WHERE transactionid = ?";
 
-        $stmt = $conn->prepare($updateSql);
-        $stmt->bind_param('ssiddi', $productName, $transactionDate, $quantity, $price, $totalAmount, $transactionID);
+    $stmt = $conn->prepare($updateSql);
+    $stmt->bind_param('ssiddi', $productName, $transactionDate, $quantity, $price, $totalAmount, $transactionID);
 
-        if ($stmt->execute()) {
-            echo "Transaction updated successfully!";
-        } else {
-            echo "Error: " . $stmt->error;
-        }
+    if ($stmt->execute()) {
+        // Display a success message and redirect to the previous page
+        echo "<script>
+                alert('Transaction updated successfully!');
+                window.location.href = 'transaction.php';
+              </script>";
+        exit();
+    } else {
+        echo "Error: " . $stmt->error;
     }
+}
 
-    $stmt->close();
-    $conn->close();
-    ?>
+$stmt->close();
+$conn->close();
+?>
+
 
 
 <!DOCTYPE html>
