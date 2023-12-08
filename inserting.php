055597fe69740199f80684be90076036842119ef
Ryuo Jhon Mark <143322665+MarkRyuo@users.noreply.github.com>
2023-12-08 20:51:17 +0800

    update here

--- inserting.php
+++ inserting.php
@@ -68,30 +68,86 @@ $conn->close();
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 
     <title>Inserting</title>
+
+    <style>
+        body {
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            font-family: Arial, sans-serif;
+        }
+
+        nav {
+            display: flex;
+            justify-content: space-around;
+            align-items: center;
+            width: 100vw;
+            height: 10vh;
+
+            box-shadow: 2px 5px 4px #ddd;
+            
+            /* outline: 1px solid black; */
+        }
+
+        nav p {
+            margin-right: 28vw;
+        }
+
+        .container {
+            margin-top: 8vh;
+            /* outline: 1px solid black; */
+
+            width: 40vw;
+            height: 56vh;
+
+            border-radius: 20px;
+            box-shadow: 4px 5px 4px #ddd;
+        }
+
+        form {
+            width: 40%;
+            height: 60vh;
+            margin: auto;
+            /* outline: 1px solid black; */
+
+            display: flex;
+            flex-direction: column;
+            flex-wrap: wrap;
+            align-content: space-evenly;
+            gap: 1vh;
+        }
+
+        input {
+            width: 20vw;
+            height: 6vh;
+        }
+    </style>
 </head>
 <body>
 
     <nav>
         <a href="transaction.php" class="btn btn-success btn-lg">Back</a>
-        <p class="display-6">Edit Transaction</p>
+        <p class="display-6">Add New Transaction</p>
     </nav>
 
-    <form action="inserting.php" method="post">
-        <h2>Add New Transaction</h2>
-        <label for="product_name">Product Name:</label>
-        <input type="text" name="product_name" required>
+    <div class="container">
+        <form action="inserting.php" method="post">
 
-        <label for="transaction_date">Transaction Date:</label>
-        <input type="date" name="transaction_date" required>
+            <label for="product_name">Product Name:</label>
+            <input type="text" name="product_name" required>
 
-        <label for="quantity">Quantity:</label>
-        <input type="number" name="quantity" required>
+            <label for="transaction_date">Transaction Date:</label>
+            <input type="date" name="transaction_date" required>
 
-        <label for="price">Price:</label>
-        <input type="number" name="price" step="0.01" required>
+            <label for="quantity">Quantity:</label>
+            <input type="number" name="quantity" required>
 
-        <input type="submit" value="Add Transaction">
-    </form>
+            <label for="price">Price:</label>
+            <input type="number" name="price" step="0.01" required>
+
+            <input type="submit" value="Add Transaction">
+        </form>
+    </div>
 
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
