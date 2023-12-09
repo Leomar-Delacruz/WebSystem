e6d59155c84430141567130a7b9813959d89f3f6
Ryuo Jhon Mark <143322665+MarkRyuo@users.noreply.github.com>
2023-12-09 08:22:45 +0800

    update here

--- supplierlist.php
+++ supplierlist.php
@@ -11,6 +11,9 @@ $result = $conn->query($sqlSelect);
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+
+
     <title>Supplier Information</title>
     <style>
         body {
@@ -113,8 +116,9 @@ $result = $conn->query($sqlSelect);
     </table>
 
     <!-- Add Supplier Button -->
-    <a href="supplier.php" class="add-supplier-btn">Add Supplier</a>
+    <a href="supplier.php" class="add-supplier-btn btn">Add Supplier</a>
 </body>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 </html>
 
 <?php
